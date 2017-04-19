07ab26ff091355c07dafc40066f5d743f97033bf
Sajid <sajidrsid@gmail.com>
2017-04-19 17:40:15 -0500

    calculate which fraction of its elements are positive, negative & zero

---
+++ src/Algorithms/Warmup/PlusMinus.java
@@ -0,0 +1,55 @@
+package Algorithms.Warmup;
+
+import java.util.Scanner;
+
+/**
+ * Created by sajid on 19-Apr-17 at 5:29 PM.
+ *
+ Given an array of integers, calculate which fraction of its elements are positive, which fraction of its elements are negative, and which fraction of its elements are zeroes, respectively. Print the decimal value of each fraction on a new line.
+
+ Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+ Input Format
+ The first line contains an integer, N, denoting the size of the array.
+ The second line contains N space-separated integers describing an array of numbers .
+
+ Output Format
+ You must print the following  lines:
+ A decimal representing of the fraction of positive numbers in the array compared to its size.
+ A decimal representing of the fraction of negative numbers in the array compared to its size.
+ A decimal representing of the fraction of zeroes in the array compared to its size.
+
+ Sample Input
+ 6
+ -4 3 -9 0 4 1
+
+ Sample Output
+ 0.500000
+ 0.333333
+ 0.166667
+ */
+public class PlusMinus {
+    public static void main(String[] args) {
+        //Read the number of integers
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        double numPositive = 0, numNegative = 0, numZero = 0;
+        for(int i = 0; i < n; i++){
+            int num = scan.nextInt();
+            if(num > 0){
+                numPositive++;
+            }
+            else if(num < 0){
+                numNegative++;
+            }
+            else
+                numZero++;
+        }
+        //Print fraction of positive numbers
+        System.out.println(numPositive/n);
+        //Print fraction of negative numbers
+        System.out.println(numNegative/n);
+        //Print fraction of zeroes
+        System.out.println(numZero/n);
+    }
+}

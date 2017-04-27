e4ed48e2d20e0073ec660554c1989e55f51f2d4e
Sajid <sajidrsid@gmail.com>
2017-04-26 20:39:26 -0500

    2D Arrays : Hourglass

--- src/Algorithms/ThirtyDaysOfCode/Day11_2DArrays.java
+++ src/Algorithms/ThirtyDaysOfCode/Day11_2DArrays.java
@@ -49,7 +49,7 @@ public class Day11_2DArrays {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int arr[][] = new int[6][6];
-        int result = 0;
+        int result = -63;
         for(int i=0; i < 6; i++){
             for(int j=0; j < 6; j++){
                 arr[i][j] = in.nextInt();
